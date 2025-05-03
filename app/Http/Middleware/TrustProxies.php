2941a44bdc8e7e6299d5a083b08c49e75bbc2b10
Njosephine <josephinenanyonga389@gmail.com>
2025-05-03 10:48:35 +0300

    enforced https

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * Set to '*' to trust all proxies (Render requires this).
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
